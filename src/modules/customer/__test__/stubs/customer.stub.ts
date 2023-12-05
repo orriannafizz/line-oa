358a0c91ab8e1543bd41eb58c5766f85181d6991
orriannafizz <orriannafizz@gmail.com>
2023-12-05 15:38:31 +0800

    test: udpate customer stub data

--- src/modules/customer/__test__/stubs/customer.stub.ts
+++ src/modules/customer/__test__/stubs/customer.stub.ts
@@ -3,20 +3,6 @@ import { CustomerEntity } from '../../entities/customer.entity';
 
 export const customerStubs: CustomerEntity[] = [
   {
-    id: 5n,
-    firstName: 'Peter',
-    lastName: 'Wang',
-    gender: Gender.MALE,
-    email: 'peter.wang@linecorp.com',
-    birthDayId: 5,
-    birthDay: {
-      id: 5,
-      year: 1950,
-      month: 12,
-      day: 22,
-    },
-  },
-  {
     id: 1n,
     firstName: 'Robert',
     lastName: 'Yen',
@@ -25,6 +11,62 @@ export const customerStubs: CustomerEntity[] = [
     birthDayId: 1,
     birthDay: {
       id: 1,
+      year: 1985,
+      month: 8,
+      day: 8,
+    },
+  },
+  {
+    id: 2n,
+    firstName: 'Cid',
+    lastName: 'Change',
+    gender: Gender.MALE,
+    email: 'john.doe@example.com',
+    birthDayId: 2,
+    birthDay: {
+      id: 2,
+      year: 1990,
+      month: 10,
+      day: 10,
+    },
+  },
+  {
+    id: 3n,
+    firstName: 'Miki',
+    lastName: 'Lai',
+    gender: Gender.FEMALE,
+    email: 'cid.change@linecorp.com',
+    birthDayId: 3,
+    birthDay: {
+      id: 3,
+      year: 1993,
+      month: 4,
+      day: 5,
+    },
+  },
+  {
+    id: 4n,
+    firstName: 'Sherry',
+    lastName: 'Chang',
+    gender: Gender.FEMALE,
+    email: 'sherry.lai@linecorp.com',
+    birthDayId: 4,
+    birthDay: {
+      id: 4,
+      year: 1993,
+      month: 8,
+      day: 8,
+    },
+  },
+  {
+    id: 5n,
+    firstName: 'Peter',
+    lastName: 'Wang',
+    gender: Gender.MALE,
+    email: 'peter.wang@linecorp.com',
+    birthDayId: 5,
+    birthDay: {
+      id: 5,
       year: 1950,
       month: 12,
       day: 22,

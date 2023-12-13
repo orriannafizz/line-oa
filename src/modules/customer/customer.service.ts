a012636b862a7e82a5b063b63463ccdf0b1ae4c0
orriannafizz <orriannafizz@gmail.com>
2023-12-13 11:10:37 +0800

    refractor: customer service more readable

--- src/modules/customer/customer.service.ts
+++ src/modules/customer/customer.service.ts
@@ -4,6 +4,7 @@ import { from } from 'rxjs';
 import { map, mergeMap, toArray } from 'rxjs/operators';
 import { CustomerEntity } from './entities/customer.entity';
 import { PrismaService } from '@/shared/prisma.service';
+import e from 'express';
 
 @Injectable()
 export class CustomerService {
@@ -14,40 +15,40 @@ export class CustomerService {
     const date = new Date();
     const month = date.getMonth() + 1;
     const day = date.getDate();
+    const customersPromise = this.prisma.customer.findMany({
+      where: {
+        birthDay: {
+          month,
+          day,
+        },
+      },
+      include: {
+        birthDay: true,
+      },
+    });
 
-    return from(
-      this.prisma.customer.findMany({
-        where: {
-          birthDay: {
-            month,
-            day,
-          },
-        },
-        include: {
-          birthDay: true,
-        },
-      }),
-    );
+    return from(customersPromise);
   }
 
   generateCongratulationMessageV3(): Observable<string> {
     const currentYear = new Date().getFullYear();
     const imageUrl =
       'https://tonsofthanks.com/wp-content/uploads/2023/08/Hot-Dog-Funny-Birthday-Meme.jpg';
-
-    return this.getTodayBirthdayCustomers().pipe(
+    const customers$ = this.getTodayBirthdayCustomers();
+    const messages$ = customers$.pipe(
       map((customers) => {
         const messages = customers.map((customer) => {
           const age = currentYear - customer.birthDay.year;
           const isSenior = age > 49;
-          let message = `Subject: Happy birthday! Happy birthday, dear \`${customer.firstName}\`!\n`;
-          if (isSenior) {
-            message += `![Happy Birthday](${imageUrl})\n`;
-          }
-          return message;
+          if (isSenior)
+            return `Subject: Happy birthday! Happy birthday, dear \`${customer.firstName}\`!\n![Happy Birthday](${imageUrl})\n`;
+          else
+            return `Subject: Happy birthday! Happy birthday, dear \`${customer.firstName}\`!\n`;
         });
         return messages.join('');
       }),
     );
+
+    return messages$;
   }
 }
